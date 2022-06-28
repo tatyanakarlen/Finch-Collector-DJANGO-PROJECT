860adadaf71e18d105cf4118a7031823807d0015
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-06-28 18:46:06 -0400

    latest

--- main_app/views.py
+++ main_app/views.py
@@ -6,60 +6,14 @@ from .forms import AirPlayForm
 
 
 
-# Create your views here.
-
-# def index(request):
-#     return render(request, 'records/index.html', { "records": records })
-
 def about(request):
-     #assumes about.html is in templates
-    #hey database, it seems the user has accessed the product id 5 
-    #please give me the data for product id 
-    return render(request, 'about.html')
-    # return HttpResponse('<h1>About the record collector</h1>')
+   return render(request, 'about.html')
 
 
-#define the home view
 def home(request):
     return render(request, 'home.html')
 
    
-
-# def seed(request):
-#     #create few records 
-#     Record.objects.create(title='Parallel Lines', 
-#                           artist= 'Blondie', 
-#                           label= 'Chrysalis', 
-#                           year= '1978', 
-#                           description = "Blondie's most polished and commercial sound to date"
-#                             )
-#     Record.objects.create(title='The Well-Tempered Clavier Book I & II: J.S.Bach', 
-#                           artist= 'Svyatoslav Richter', 
-#                           label= 'Melodiya', 
-#                           year= '1971', 
-#                           description = "Legendary recording of one of Bach's greatest works by renowed Russian pianist"
-#                             )
-#     Record.objects.create(title='Love To Love You Baby', 
-#                           artist= 'Donna Summer', 
-#                           label= 'Casablanca', 
-#                           year= '1975', 
-#                           description = "Sleazy disco classic, on heavy rotation at Studio 54"
-#                             )
-#     Record.objects.create(title='Rebel Yell', 
-#                           artist= 'Billy Idol', 
-#                           label= 'Chrysalis', 
-#                           year= '1983', 
-#                           description = "London punk Billy Idol is back with a rock/new-wave album"
-#                             )
-#     Record.objects.create(title='Madonna', 
-#                           artist= 'Madonna', 
-#                           label= 'SIRE', 
-#                           year= '1983', 
-#                           description = "New artist Madonna blows everyone away with fresh club/pop sound"
-#                             )
-#     return HttpResponse('done')
-
-
 def records_index(request):
     records = Record.objects.all()
     return render(request, 
@@ -84,11 +38,6 @@ class RecordUpdate(UpdateView):
   success_url = '/records/'
   fields = '__all__'
     
-# def show(request, record_id):
-#         print('incoming wildcard value is' , record_id)
-#         #hey database, please get me the record with id=2
-#         record = Record.objects.get(id=record_id)
-#         return render(request, 'records/detail.html', {'record': record})
 
 def records_detail(request, record_id):
   record = Record.objects.get(id=record_id)
